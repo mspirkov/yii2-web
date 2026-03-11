7e4e7ba6573262bc5a8bfab1b82493ce1cd9a024
Максим Спирков <spirkov.2001@mail.ru>
2026-03-11 18:30:20 +0400

    feat: add `CookieManagerInterface` and mark `CookieManager` as `final`

--- src/CookieManager.php
+++ src/CookieManager.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace MSpirkov\Yii2\Web;
 
-use DateTimeInterface;
 use yii\web\Response;
 use yii\web\Cookie;
 use yii\web\Request;
@@ -18,11 +17,11 @@ use yii\web\Request;
  *
  * It contains the following methods:
  *
- * - {@see CookieManager::has()} - checks if a cookie with the specified name exists.
- * - {@see CookieManager::get()} - returns the cookie with the specified name.
- * - {@see CookieManager::add()} - adds a cookie to the response.
- * - {@see CookieManager::remove()} - removes a cookie.
- * - {@see CookieManager::removeAll()} - removes all cookies.
+ * - {@see CookieManagerInterface::has()} - checks if a cookie with the specified name exists.
+ * - {@see CookieManagerInterface::get()} - returns the cookie with the specified name.
+ * - {@see CookieManagerInterface::add()} - adds a cookie to the response.
+ * - {@see CookieManagerInterface::remove()} - removes a cookie.
+ * - {@see CookieManagerInterface::removeAll()} - removes all cookies.
  *
  * Usage example:
  *
@@ -35,20 +34,9 @@ use yii\web\Request;
  *
  * @author Maksim Spirkov <spirkov.2001@mail.ru>
  *
- * @phpstan-type CookieData array{
- *     name: string,
- *     value: string,
- *     expire?: int|string|DateTimeInterface|null,
- *     path?: string,
- *     domain?: string,
- *     secure?: bool,
- *     httpOnly?: bool,
- *     sameSite?: string,
- * }
- *
  * @immutable
  */
-class CookieManager
+final class CookieManager implements CookieManagerInterface
 {
     private Request $request;
 
@@ -60,21 +48,6 @@ class CookieManager
         $this->response = $response;
     }
 
-    /**
-     * Checks if a cookie with the specified name exists.
-     *
-     * The cookie can be checked in the Request object (normal behavior) or directly in
-     * the `$_COOKIE` superglobal array.
-     *
-     * Note that if a cookie is marked for deletion from browser or its value is an empty
-     * string, this method will return `false`.
-     *
-     * @param string $name The name of the cookie to check.
-     * @param bool $inRequest Determines whether to look for the cookie in the Request
-     * object or directly in `$_COOKIE`.
-     *
-     * @return bool Whether the named cookie exists.
-     */
     public function has(string $name, bool $inRequest = true): bool
     {
         if ($inRequest) {
@@ -86,32 +59,11 @@ class CookieManager
         return $cookieValue !== null && $cookieValue !== '';
     }
 
-    /**
-     * Returns the cookie with the specified name.
-     *
-     * The cookie can be retrieved from the Request object (normal behavior) or directly
-     * from the `$_COOKIE` superglobal array.
-     *
-     * @param string $name The name of the cookie to retrieve.
-     * @param bool $fromRequest Determines whether to retrieve the cookie from the Request
-     * object or directly from `$_COOKIE`.
-     *
-     * @return Cookie|null The cookie with the specified name. Null if the named cookie does
-     * not exist.
-     */
     public function get(string $name, bool $fromRequest = true): ?Cookie
     {
         return $fromRequest ? $this->request->cookies->get($name) : $this->getDirectly($name);
     }
 
-    /**
-     * Adds a cookie to the response.
-     *
-     * If there is already a cookie with the same name in the collection, it will be removed first.
-     *
-     * @param Cookie|CookieData $data The cookie to be added. It can be a `Cookie` object
-     * or a `CookieData` array.
-     */
     public function add($data): void
     {
         $cookie = is_array($data) ? new Cookie($data) : $data;
@@ -119,23 +71,11 @@ class CookieManager
         $this->response->cookies->add($cookie);
     }
 
-    /**
-     * Removes a cookie.
-     *
-     * If `$removeFromBrowser` is `true`, the cookie will be removed from the browser.
-     * In this case, a cookie with outdated expiry will be added to the collection.
-     *
-     * @param Cookie|string $cookie The cookie object or the name of the cookie to be removed.
-     * @param bool $removeFromBrowser Whether to remove the cookie from browser.
-     */
     public function remove($cookie, bool $removeFromBrowser = true): void
     {
         $this->response->cookies->remove($cookie, $removeFromBrowser);
     }
 
-    /**
-     * Removes all cookies.
-     */
     public function removeAll(): void
     {
         $this->response->cookies->removeAll();
