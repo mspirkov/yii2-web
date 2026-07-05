cfd5194d8c871802992956e9b65c16684ab58f9c
Максим Спирков <spirkov.2001@mail.ru>
2026-07-05 14:45:35 +0400

    chore: update Rector config

--- rector.php
+++ rector.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\CodingStyle\Rector\ArrowFunction\StaticArrowFunctionRector;
-use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\Config\RectorConfig;
 use Rector\Instanceof_\Rector\Ternary\FlipNegatedTernaryInstanceofRector;
 use Rector\Set\ValueObject\SetList;
@@ -26,6 +24,4 @@ return RectorConfig::configure()
     ])
     ->withRules([
         FlipNegatedTernaryInstanceofRector::class,
-        StaticClosureRector::class,
-        StaticArrowFunctionRector::class,
     ]);
