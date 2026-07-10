011a2f83a06f3463c9fd47b252c6b858b674b96d
Максим Спирков <spirkov.2001@mail.ru>
2026-07-10 17:10:03 +0400

    chore: update Rector config

--- rector.php
+++ rector.php
@@ -2,9 +2,23 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\BooleanAnd\SimplifyEmptyArrayCheckRector;
+use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
+use Rector\CodeQuality\Rector\FuncCall\SortCallLikeNamedArgsRector;
+use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
+use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
+use Rector\CodeQuality\Rector\Isset_\IssetOnPropertyObjectToPropertyExistsRector;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\PropertyProperty\RemoveNullPropertyInitializationRector;
 use Rector\Instanceof_\Rector\Ternary\FlipNegatedTernaryInstanceofRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Set\ValueObject\SetList;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\TypeDeclaration\Rector\Empty_\EmptyOnNullableObjectToInstanceOfRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -24,4 +38,24 @@ return RectorConfig::configure()
     ])
     ->withRules([
         FlipNegatedTernaryInstanceofRector::class,
+    ])
+    ->withSkip([
+        // PHP 7.4
+        ClosureToArrowFunctionRector::class,
+        // Code Quality
+        DisallowedEmptyRuleFixerRector::class,
+        ExplicitBoolCompareRector::class,
+        FlipTypeControlToUseExclusiveTypeRector::class,
+        IssetOnPropertyObjectToPropertyExistsRector::class,
+        SimplifyEmptyArrayCheckRector::class,
+        SimplifyEmptyCheckOnEmptyArrayRector::class,
+        SortCallLikeNamedArgsRector::class,
+        SimplifyIfElseToTernaryRector::class,
+        SimplifyIfReturnBoolRector::class,
+        // Dead Code
+        SimplifyUselessVariableRector::class,
+        RemoveNullPropertyInitializationRector::class,
+        RemoveUselessParamTagRector::class,
+        // Type Declarations
+        EmptyOnNullableObjectToInstanceOfRector::class,
     ]);
